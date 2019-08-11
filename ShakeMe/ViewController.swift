96f5666c30a9b3a2f28e53d0099f2914e2682510
Martin Höller <mhoeller81@gmail.com>
2019-08-11 13:31:03 +0200

    Initial Commit

---
+++ ShakeMe/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ShakeMe
+//
+//  Created by Martin Höller on 11.08.19.
+//  Copyright © 2019 blue banana software. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
